4ef7a293f56c64d4230e36066a435171452c1fe7
Faisal Gani <40407132+IsalGani@users.noreply.github.com>
2023-12-19 19:31:34 +0800

    remove Desa, Proyek, Tahun Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             UserSeeder::class,
-            TahunSeeder::class,
-            DesaSeeder::class,
-            ProyekSeeder::class,
+           
         ]);
     }
 }
